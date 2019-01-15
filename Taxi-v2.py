e231a7508e7e58cbd0ca899bce257aae7b5f9528
Nazia Habib <habib.nazia@gmail.com>
2019-01-15 10:46:09 -0600

    Create Taxi-v2.py

---
+++ Taxi-v2.py
@@ -0,0 +1,11 @@
+#set up gym
+
+import gym
+env = gym.make('Taxi-v2')
+env.reset()
+
+#create a game loop and take random actions
+
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) 
