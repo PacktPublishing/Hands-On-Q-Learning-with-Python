63d8a2bfded91abdd5261fabb551b49632c0b671
Nazia Habib <habib.nazia@gmail.com>
2019-03-17 14:22:15 -0500

    Update CartPole.py

--- ch6/CartPole.py
+++ ch6/CartPole.py
@@ -3,10 +3,9 @@
 
 # In[72]:
 
-import random
 import gym
 import numpy as np
-from collections import deque
+import random
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
