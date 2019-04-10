2dd74f83a2348273ebbe8691e8a5466bbdf8c86a
Nazia Habib <habib.nazia@gmail.com>
2019-04-10 13:13:39 -0500

    Update bandit.py

--- ch7/bandit.py
+++ ch7/bandit.py
@@ -1,1 +1,15 @@
-#bandit
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import random
+
+total_views = 10000
+num_ads = 5
+ad_list = []
+total_reward = 0
+for view in range(total_views):
+    ad = random.randrange(num_ads)
+    ad_list.append(ad)
+    reward = dataset.values[view, ad]
+    total_reward = total_reward + reward
