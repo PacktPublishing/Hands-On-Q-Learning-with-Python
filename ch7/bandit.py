6447d5f1b2549251dae490852ac8a23ba93ac76d
Nazia Habib <habib.nazia@gmail.com>
2019-04-11 15:39:11 -0500

    Update bandit.py

--- ch7/bandit.py
+++ ch7/bandit.py
@@ -13,5 +13,5 @@ total_reward = 0
 for view in range(total_views):
     ad = random.randrange(num_ads)
     ad_list.append(ad)
-#    reward = dataset.values[view, ad]
-#    total_reward = total_reward + reward
+    reward = dataset.values[view, ad]
+    total_reward = total_reward + reward
