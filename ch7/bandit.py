9ed82b822f658c107735023dfd2b8bff177f8985
Nazia Habib <habib.nazia@gmail.com>
2019-04-11 15:37:30 -0500

    Update bandit.py

--- ch7/bandit.py
+++ ch7/bandit.py
@@ -1,8 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import random
+
+df = pd.DataFrame(np.random.choice(np.arange(0, 2), p=[0.9, 0.1],size=(10000, 5)), columns=list('ABCDE'))
+
 
 total_views = 10000
 num_ads = 5
@@ -11,5 +13,5 @@ total_reward = 0
 for view in range(total_views):
     ad = random.randrange(num_ads)
     ad_list.append(ad)
-    reward = dataset.values[view, ad]
-    total_reward = total_reward + reward
+#    reward = dataset.values[view, ad]
+#    total_reward = total_reward + reward
